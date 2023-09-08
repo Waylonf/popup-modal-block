2b8e37a4e89be7a3ebc28cd6b4d8272ed1c49604
Waylon <waylonf@gmail.com>
2023-09-08 09:18:37 +0300

    test

--- src/edit.js
+++ src/edit.js
@@ -1,8 +1,3 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
 import { __ } from '@wordpress/i18n';
 
 /**
